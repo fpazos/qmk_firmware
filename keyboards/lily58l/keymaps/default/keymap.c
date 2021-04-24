afb5259ef08b8a559b2bf7aaccd6048625305034
Fernando Pazos Estévez <30479687+fpazos@users.noreply.github.com>
2021-04-24 17:18:17 +0200

    Changed keymap

--- keyboards/lily58l/keymaps/default/keymap.c
+++ keyboards/lily58l/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, \
-  KC_TAB, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_NUBS,  KC_INS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  MT(MOD_LSFT,KC_ENT), \
      KC_LCTRL, MT(MOD_LALT,KC_LGUI), LT(_LOWER, KC_SPC),KC_BSPC,  KC_SPC, LT(_RAISE, KC_BSPC),  MT(MOD_RALT,KC_LEFT), KC_RIGHT \
 ),
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | F12  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |   |  |   &  |   !  | CtF5 |                    |      |Print1|      |      |      | `^[  |
+ * |      |      |   |  |   &  |   !  | CtF5 |                    |      |Print1|  #   |  @   |      | `^[  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   ~  |   {  |   [  |   (  |   '  |-------.    ,-------|   "  |   )  |   ]  |   }  |      | çÇ}  |
  * |------+------+------+------+------+------|   \   |    | Print |------+------+------+------+------+------|
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_F12, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                              KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_CAPS, _______, ALGR(KC_1), KC_CIRC, RSFT(KC_1), C(KC_F5),                                  _______, RALT(KC_PSCR), _______, _______, KC_EQL, KC_LBRC,\
+  KC_CAPS, _______, ALGR(KC_1), KC_CIRC, RSFT(KC_1), C(KC_F5),                                  _______, RALT(KC_PSCR), RALT(KC_3), RALT(KC_2), KC_EQL, KC_LBRC,\
   _______, RALT(KC_4), RALT(KC_QUOT),RALT(KC_LBRC),RSFT(KC_8),KC_MINS,                            KC_AT, RSFT(KC_9), ALGR(KC_RBRC), ALGR(KC_NUHS), _______, KC_NUHS, \
   _______, RSFT(KC_5), RSFT(KC_RBRC), KC_SLSH, KC_RBRC, RSFT(KC_7), ALGR(KC_GRV),      KC_PSCR, C(KC_S), C(KC_Z), C(KC_Y), C(KC_V), C(KC_C), C(KC_X), \
                                     _______, _______, _______, KC_DEL,                    _______, _______, _______, _______\
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | MRwd | Stop | Play | MFwd |      |                    |      |      |  Up  |      |      |      |
+ * |      | MPrv | Stop | Play | MNxt |      |                    |      |      |  Up  |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      | PagUp| Home | End  | PagDw|      |-------.    ,-------|      | Left | Down | Right|      |      |
  * |------+------+------+------+------+------|       |    |ctaltsu|------+------+------+------+------+------|
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, KC_EQL, \
-  _______,  KC_MRWD, KC_MSTP, KC_MPLY, KC_MFFD,_______,                           _______, _______, KC_UP  , _______, _______, _______, \
+  _______,  KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT,_______,                           _______, _______, KC_UP  , _______, _______, _______, \
   _______,  KC_PGUP, KC_HOME, KC_END, KC_PGDN, _______,                           _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
   _______,  _______, KC_MUTE, KC_VOLD, KC_VOLU,_______,  _______,     LCA(KC_DEL),_______, C(KC_INS), RSFT(KC_INS), _______, _______, _______, \
                              _______, _______, _______,  _______,       _______,  _______, _______, _______ \
